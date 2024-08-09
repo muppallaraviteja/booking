eab5c4e94ec82908edc0793b9a84fa5d494ffd7d
Ravi Teja muppalla <153738634+bonitoravi@users.noreply.github.com>
2024-08-09 16:50:14 +0530

    Seat unavailable exception  working

--- src/main/java/com/ravi/booking/strategy/RandomStrategy.java
+++ src/main/java/com/ravi/booking/strategy/RandomStrategy.java
@@ -6,15 +6,21 @@ import java.util.*;
 
 public class RandomStrategy implements SeatSelectionStrategy {
 
-  private static final int MAX_SEATS = 50;
+  private static final int MAX_SEATS = 1;
   private final Random random = new Random();
 
   @Override
   public int selectSeat(Section section, Set<Integer> allocatedSeats) {
+    if (allocatedSeats.size() >= MAX_SEATS) {
+      // All seats are taken, return -1 to indicate no available seats
+      return -1;
+    }
+
     int seat;
     do {
       seat = random.nextInt(MAX_SEATS) + 1;
     } while (allocatedSeats.contains(seat));
+
     allocatedSeats.add(seat);
     return seat;
   }
